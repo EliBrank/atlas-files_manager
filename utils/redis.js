882074ff2737c908c37a553c1a67a5decbac2de5
EliBrank <ezb.business@pm.me>
2025-03-17 19:21:01 +0000

    update redis in package.json, try adjusting connection settings in redis class

--- utils/redis.js
+++ utils/redis.js
@@ -1,56 +1,64 @@
 import { createClient } from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
-        this.connected = false;
+  constructor() {
+    this.client = createClient();
+    this.connected = false;
 
-        this.client.on('error', error => {
-            console.log('Redis Client Error', error);
-        });
+    this.client.on('error', error => {
+      console.log('Redis Client Error', error);
+    });
 
-        this.client.on('ready', () => {
-            this.connected = true;
-            console.log('redis connected successfully');
-        });
+    this.client.on('ready', () => {
+      this.connected = true;
+      console.log('redis connected successfully');
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async connect() {
+      try {
+          await this.client.connect();
+      } catch (error) {
+          console.error('Failed to connect to Redis:', error);
+      }
+  }
+
+  async get(key) {
+    if (!this.isAlive()) return null;
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, result) => {
+        if (error) reject(error);
+          else resolve(result);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    if (!this.isAlive()) {
+      console.error('Error Connecting to Data Storage')
+      return null;
     }
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (error, result) => {
+        if (error) reject(error);
+          else resolve(result);
+      });
+    });
+  }
 
-    isAlive() {
-        return this.connected;
-    }
-
-    async get(key) {
-        if (!this.isAlive()) return null;
-        return new Promise((resolve, reject) => {
-            this.client.get(key, (error, result) => {
-                if (error) reject(error);
-                else resolve(result);
-            });
-        });
-    }
-
-    async set(key, value, duration) {
-        if (!this.isAlive()) {
-            console.error('Error Connecting to Data Storage')
-            return null;
-        }
-        return new Promise((resolve, reject) => {
-            this.client.set(key, value, 'EX', duration, (error, result) => {
-                if (error) reject(error);
-                else resolve(result);
-            });
-        });
-    }
-
-    async del(key) {
-        if (!this.isAlive()) return null;
-        return new Promise((resolve, reject) => {
-            this.client.del(key, (error, result) => {
-                if (error) reject(error);
-                else resolve(result);
-            });
-        });
-    }
+  async del(key) {
+    if (!this.isAlive()) return null;
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, result) => {
+        if (error) reject(error);
+          else resolve(result);
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
