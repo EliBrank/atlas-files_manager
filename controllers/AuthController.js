5226ebbfb3f4e6ff1e1c31bc8ed4173d3950004c
EliBrank <ezb.business@pm.me>
2025-03-19 14:26:03 -0500

    create AuthController, add related endpoints to routes

---
+++ controllers/AuthController.js
@@ -0,0 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+class AuthController {
+    static async getConnect() {
+        // TODO fill out
+    }
+}
+
+export default AuthController;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import AppController from "../controllers/AppController.js";
+import AuthController from "../controllers/AuthController.js";
 
 const routes = express.Router();
 
 routes.get('/status', AppController.getStatus);
-
 routes.get('/stats', AppController.getStats);
+routes.get('/connect', AuthController.getConnect)
 
 export default routes;
