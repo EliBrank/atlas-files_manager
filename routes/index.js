1a18552f2f3ef3478a60b8f7ea495bfd61fb6c6f
Greentaylor27 <greentaylor18@gmail.com>
2025-03-24 11:59:00 -0500

    Spent maybe an hour working on this today 3/24/25 and it seems to be working finally as intended so I'm pushing all the files that have updates
    added the middleware app.use(express.json()); to allow user input or email and password
    
    updated indx.js to allow routing to '/users' to add a user
    
    updated UsersController to finally allow the user to be added to the database and return the results in a json (the Id, and the email.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import AppController from "../controllers/AppController.js";
+import UsersController from "../controllers/UsersContoller.js";
 
 const routes = express.Router();
 
 routes.get('/status', AppController.getStatus);
 
 routes.get('/stats', AppController.getStats);
+
+routes.post('/users', UsersController.postNew);
 
 export default routes;
