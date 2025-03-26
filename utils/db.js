1559a9385f6c5841ea80462a2e0b8b32d3ab06d8
EliBrank <ezb.business@pm.me>
2025-03-26 15:06:19 -0500

    test FilesController functionality, update db utils to better handle authentication checks

--- utils/db.js
+++ utils/db.js
@@ -1,55 +1,90 @@
 import pkg from 'mongodb';
-const { MongoClient } = pkg;
+import redisClient from './redis';
+const { MongoClient, ObjectId } = pkg;
 
 class DBClient {
-  constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || '27017';
-    const database = process.env.DB_DATABASE || 'file_manager';
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || '27017';
+        const database = process.env.DB_DATABASE || 'file_manager';
 
-    const url = `mongodb://${host}:${port}`;
+        const url = `mongodb://${host}:${port}`;
 
-    // set default for connected status
-    this.connected = false;
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
+        // set default for connected status
+        this.connected = false;
+        this.client = new MongoClient(url, { useUnifiedTopology: true });
 
-    this.client.connect()
-      .then(() => {
-        this.connected = true;
-        this.db = this.client.db(database);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  isAlive() {
-    return this.connected;
-  }
-
-  async nbUsers() {
-    // no users returned if database isn't running or other issue
-    if (!this.isAlive()) return 0;
-    try {
-      const users = this.db.collection('users');
-      // countDocuments is asynchronous
-      return await users.countDocuments();
-    } catch (error) {
-      console.error(error);
-      return 0;
+        this.client.connect()
+            .then(() => {
+                this.connected = true;
+                this.db = this.client.db(database);
+            })
+            .catch((error) => {
+                console.error(`Error establishing connection to DB: ${error}`);
+            });
     }
-  }
 
-  async nbFiles() {
-    if (!this.isAlive()) return 0;
-    try {
-      const files = this.db.collection('files');
-      return await files.countDocuments();
-    } catch (error) {
-      console.error(error);
-      return 0;
+    isAlive() {
+        return this.connected;
     }
-  }
+
+    async nbUsers() {
+        // no users returned if database isn't running or other issue
+        if (!this.isAlive()) return 0;
+        try {
+            const users = this.db.collection('users');
+            // countDocuments is asynchronous
+            return await users.countDocuments();
+        } catch (error) {
+            console.error(`Error counting users in DB: ${error}`);
+            return 0;
+        }
+    }
+
+    async nbFiles() {
+        if (!this.isAlive()) return 0;
+        try {
+            const files = this.db.collection('files');
+            return await files.countDocuments();
+        } catch (error) {
+            console.error(`Error counting files in DB: ${error}`);
+            return 0;
+        }
+    }
+
+    async validateToken(token) {
+        if (!this.isAlive() || !token) return 0;
+        try {
+            return await redisClient.get(`auth_${token}`);
+        } catch (error) {
+            console.error(`Error validating Redis token: ${error}`);
+            return 0;
+        }
+    }
+
+    async getUserById(id) {
+        if (!this.isAlive() || !id) return 0;
+        try {
+            return await this.db.collection('users').findOne({
+                _id: new ObjectId(id),
+            });
+        } catch (error) {
+            console.error(`Error getting user from DB: ${error}`);
+            return 0;
+        }
+    }
+
+    async authenticateUser(token) {
+        if (!this.isAlive() || !token) return 0;
+        try {
+            const userId = await this.validateToken(token);
+            if (!userId) return 0;
+            return this.getUserById(userId);
+        } catch (error) {
+            console.error(`Error authenticating user: ${error}`);
+            return 0;
+        }
+    }
 }
 
 const dbClient = new DBClient;
